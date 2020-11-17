18e54f2511467ca612957db5bc143d36433e0eeb
kumarith <t.kumari59@gmail.com>
2020-11-17 17:03:25 +0100

    final commit

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -13,15 +13,15 @@ const PostItem = ({
   post: { _id, text, name, avatar, user, likes, comments, date },
   showActions,
 }) => (
-  <div class="post bg-white my-1 p-1">
+  <div className="post bg-white my-1 p-1">
     <div>
       <Link to={`/profile/${user}`}>
-        <img class="round-img" src={avatar} alt="" />
+        <img className="round-img" src={avatar} alt="" />
         <h4>{name}</h4>
       </Link>
     </div>
     <div>
-      <p class="my-1">{text}</p>
+      <p className="my-1">{text}</p>
       <p className="post-date">
         Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
       </p>
@@ -32,7 +32,7 @@ const PostItem = ({
             type="button"
             className="btn btn-light"
           >
-            <i class="fas.fa-thumbs-up" />{" "}
+            <i className="fas fa-thumbs-up" />{" "}
             <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
           </button>
           <button
@@ -40,7 +40,7 @@ const PostItem = ({
             type="button"
             className="btn btn-light"
           >
-            <i class="fas fa-thumbs-down"></i>
+            <i className="fas fa-thumbs-down"></i>
           </button>
           <Link to={`/posts/${_id}`} className="btn btn-primary">
             Discussion{" "}
