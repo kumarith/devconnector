1dccbf6c69031e751385cc2f60eb1fd18e1a1e95
kumarith <t.kumari59@gmail.com>
2020-11-23 20:27:29 +0100

    dependecies

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -4,7 +4,7 @@ import Register from "../auth/Register";
 import Login from "../auth/Login";
 import Alert from "../layout/Alert";
 import Dashboard from "../dashboard/Dashboard";
-import ProfileForm from "../profile-forms/ProfileForms";
+import ProfileForm from "../profile-forms/ProfileForm";
 import AddExperience from "../profile-forms/AddExperience";
 import AddEducation from "../profile-forms/AddEducation";
 import Profiles from "../profiles/Profiles";
